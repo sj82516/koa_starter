3d6f12d4c29852834591864a55b230d8d9e0c588
zhengyuanjie <sj82516@gmail.com>
2017-09-27 14:37:45 +0800

    加入session

--- config/local.js
+++ config/local.js
@@ -1,3 +1,14 @@
 module.exports = {
-    port: "3000"
+    port: "3000",
+
+    session: {
+        redis: {
+            host: 'localhost',
+            port: 6379,
+            //ttl: 30 * 24 * 60 * 60 * 1000,
+            db: 12,
+            pass: '',
+            prefix: 'starter:'
+        }
+    },
 }
--- readme.md
+++
@@ -1,3 +0,0 @@
-## 個人Koa快速搭建模組  
-含router / ratelimiter / bodyparser / multer  
-不含Database
